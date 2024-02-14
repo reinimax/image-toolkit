55ffb4ec21e62b570da4f5b42b23e0ea6941cb52
Reinhold Schachner <reinhold.schachner@hotmail.com>
2024-02-14 11:02:38 +0100

    Begin working on the API, decoding and encoding the provided "image".

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+import base64
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# TODO this should go into its own file(s) and become extensible
+ALLOWED_OPERATIONS = ["resize"]
+
+def resize(original_image, width=None, height=None):
+    """Resize the image to the provided dimensions
+    
+    If both width and height are given, the image will be resized 
+    regardless of aspect ratio.
+    If either width or height are given, the image will be scaled to 
+    that dimension and aspect ratio will be maintained.
+    If the given dimension(s) are bigger tham the original image, or 
+    neither width nor heigth are provided, the original image will be 
+    returned without resizing.
+    """
+
+@app.route("/image-process", methods=["POST"])
+def image_process():
+    payload = request.json
+    operations = payload["operations"]
+    original_image_decoded = base64.b64decode(payload["original_image"])
+    with open("tmp", "wb+") as image:
+        image.write(original_image_decoded)
+
+    # for operation in operations:
+        
+        # rewind file pointer to beginning of the file.
+        image.seek(0)
+
+        return {
+            "processed_image": base64.b64encode(image.read()).decode()
+        }
