d306e1bfcb94c585ea7083e07b57b970d36d0ee7
Reinhold Schachner <reinhold.schachner@hotmail.com>
2024-02-14 14:37:57 +0100

    First "working" version of the API, allowing resizing via width in pixels. Obviously, this needs a lot of validation and refactoring, and different options for resizing need to be provided.

--- app.py
+++ app.py
@@ -4,10 +4,23 @@ from PIL import Image
 
 app = Flask(__name__)
 
-# TODO this should go into its own file(s) and become extensible
-ALLOWED_OPERATIONS = ["resize"]
+# TODO move this helper
+def parse_dimension(str):
+    """Parse a string representing an image dimension into value and unit.
+    
+    The first part of the string must be numeric, and the latter part must 
+    be a unit of either pixels ("px") oder percent ("%").
+    Returns a tuple of (value, unit) or None if the string could not be parsed.
+    """
+    str = str.lower()
+    if str.endswith("px"):
+        # TODO make this an enum?
+        unit = "px"
+        value = int(str[:-2])
+    return (value, unit)
 
-def resize(original_image, width=None, height=None):
+
+def resize(image, width=None, height=None):
     """Resize the image to the provided dimensions
     
     If both width and height are given, the image will be resized 
@@ -18,6 +31,21 @@ def resize(original_image, width=None, height=None):
     neither width nor heigth are provided, the original image will be 
     returned without resizing.
     """
+    width, unit = parse_dimension(width)
+    # Get original image dimensions
+    orig_w, orig_h = image.size
+    # Calculate by how much the image scales down
+    scale = width / orig_w
+    # Scale the not-provided dimension accordingly
+    height = int(orig_h * scale)
+
+    return image.resize((width, height))
+
+
+# TODO this should go into its own file(s) and become extensible
+ALLOWED_OPERATIONS = {
+    "resize": resize
+}
 
 @app.route("/image-process", methods=["POST"])
 def image_process():
@@ -29,9 +57,10 @@ def image_process():
     raw_image = io.BytesIO(original_image_decoded)
     processed_image = io.BytesIO()
     image = Image.open(raw_image)
+    for operation in operations:
+        if operation["name"].lower() in ALLOWED_OPERATIONS:
+            image = ALLOWED_OPERATIONS[operation["name"]](image, operation["width"])
     image.save(processed_image, format="jpeg")
-    # for operation in operations:
-        
     procsessed_image_encoded = base64.b64encode(processed_image.getvalue()).decode()
     raw_image.close()
     processed_image.close()
