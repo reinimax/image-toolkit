d04addb958f407585336b46592e1fef89119f379
Reinhold Schachner <reinhold.schachner@hotmail.com>
2024-02-15 15:44:17 +0100

    Dynamically find out what args each operation needs, and pass them in.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import base64, io, binascii
+import base64, io, binascii, inspect
 from PIL import Image
 from operations import ALLOWED_OPERATIONS
 
@@ -21,19 +21,34 @@ def image_process():
     operations = payload.get("operations", [])
     if not isinstance(operations, list):
         return {"error": "Operations must be a list."}
-    # Load the bytes of the decoded image directly into memory, without 
-    # creating a temporary file.
+    # Load the bytes of the decoded image directly into memory, without creating a temporary file.
     raw_image = io.BytesIO(original_image_decoded)
-    processed_image = io.BytesIO()
     image = Image.open(raw_image)
     for operation in operations:
         # Ignore keys that don't follow the format of a dict.
         if not isinstance(operation, dict):
             continue
         if operation.get("name", '').lower() in ALLOWED_OPERATIONS:
-            # TODO find a way to pass in the parameters the respective function needs.
-            # Or pass in everything and let the function figure it out?
-            image = ALLOWED_OPERATIONS[operation["name"]](image, operation["width"])
+            # Get the function from the allowed operations dict.
+            func = ALLOWED_OPERATIONS[operation["name"]]
+            # Get a list of the arguments it takes.
+            argslist = inspect.getfullargspec(func)[0]
+            # Remove the olbigatory image parameter.
+            argslist.remove("image")
+            # For each argument that the function expects, add it to a dictionary if it was 
+            # provided to the API.
+            provided_args = {}
+            for arg in argslist:
+                if (operation.get(arg)):
+                    provided_args[arg] = operation.get(arg)
+            # Call the requested function. If anything goes wrong, we just skip the operation.
+            try:
+                # Use the unpacking operator to pass the named arguments to the function from the dict.
+                image = func(image, **provided_args)
+            except:
+                continue
+    # Create a buffer in memory and save the processed image to it.
+    processed_image = io.BytesIO()
     image.save(processed_image, format="jpeg")
     width, height = image.size
     procsessed_image_encoded = base64.b64encode(processed_image.getvalue()).decode()
