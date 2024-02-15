8308cc7968e9623ebc3baa3ccc0ad79a3fa71c32
Reinhold Schachner <reinhold.schachner@hotmail.com>
2024-02-15 16:26:51 +0100

    Make the unit an enum and support both percent and pixels in parse_dimensions.

--- helpers.py
+++ helpers.py
@@ -1,4 +1,9 @@
 from typing import Tuple
+from enum import Enum, auto
+
+class Unit(Enum):
+    PERCENT = auto()
+    PIXELS = auto()
 
 def parse_dimension(str: str) -> Tuple[int, str]:
     """Parse a string representing an image dimension into value and unit.
@@ -7,9 +12,12 @@ def parse_dimension(str: str) -> Tuple[int, str]:
     be a unit of either pixels ("px") oder percent ("%").
     Returns a tuple of (value, unit) or None if the string could not be parsed.
     """
-    str = str.lower()
-    if str.endswith("px"):
-        # TODO make this an enum?
-        unit = "px"
-        value = int(str[:-2])
+    if str.endswith("%"):
+        unit = Unit.PERCENT
+    # If no percent was given, assume pixels.
+    else:
+        unit = Unit.PIXELS
+    # Neat trick to remove all non-digits from string.
+    # See https://www.tutorialspoint.com/How-to-remove-characters-except-digits-from-string-in-Python
+    value = int("".join(i for i in str if i.isdigit()))
     return (value, unit)
