e158167dc78673ef03129c61bff7b54bca5015e6
Reinhold Schachner <reinhold.schachner@hotmail.com>
2024-02-15 13:42:57 +0100

    Move the pdoc navbar and minimally style it.

--- docs/custom.js
+++ docs/custom.js
@@ -1,2 +1,10 @@
-// TODO move pdoc nav
-console.log("hi")
+function movePdocNavBar(e) {
+    navbar = document.querySelector('nav.pdoc');
+    navbar.querySelector('.memberlist').classList.add('md-nav__list');
+    navbar.querySelectorAll('.memberlist a').forEach(a => a.classList.add('md-nav__link'));
+
+    sidebar = document.querySelector('.md-sidebar--secondary .md-sidebar__inner')
+    sidebar.innerHTML = navbar.outerHTML;
+    navbar.remove();
+}
+document.addEventListener("DOMContentLoaded", movePdocNavBar);
