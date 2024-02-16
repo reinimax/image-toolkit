9a3fee065b3740e5f35dc02adac3ad346b5638d7
Reinhold Schachner <reinhold.schachner@hotmail.com>
2024-02-16 15:25:56 +0100

    Add image resize to the frontend.

--- frontend/script.js
+++ frontend/script.js
@@ -9,6 +9,8 @@ function addDataUrlPrefix(str, mimeType) {
 function previewImage(base64Str, mimeType) {
     const preview = document.querySelector("#image-preview");
     preview.src = addDataUrlPrefix(base64Str, mimeType);
+    // Save mime type for later.
+    preview.dataset.mimeType = mimeType;
 }
 
 function handleImageUpload(e) {
@@ -27,6 +29,47 @@ function handleImageUpload(e) {
     });
 }
 
+async function makeRequest(operation) {
+    // Get the image from the preview
+    const img = document.querySelector("#image-preview")
+    const mimeType = img.dataset.mimeType
+    const imgBase64 = removeDataUrlPrefix(img.src, mimeType)
+    // Build the payload
+    const payload = {
+        "operations": [
+            operation
+        ],
+        "original_image": imgBase64
+    }
+    // Actually make the request to the API
+    // TODO change this to the real URL 
+    const response = await fetch('http://127.0.0.1:5000/image-process', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload)
+    });
+    return await response.json();
+}
+
+async function resize(e) {
+    e.preventDefault();
+    // Get formdata, see https://stackoverflow.com/a/66407161
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    const size = formProps.size + formProps.unit
+    const dimension = `${formProps.dimension}`;
+    const operation = {
+        "name": "resize",
+    }
+    operation[dimension] = size;
+    const response = await makeRequest(operation);
+    const mimeType = "image/" + response.metadata.format;
+    previewImage(response.processed_image, mimeType);
+}
+
 document.addEventListener("DOMContentLoaded", e => {
     document.querySelector("#image-upload-form").addEventListener("submit", handleImageUpload)
+    document.querySelector("#resize").addEventListener("submit", resize)
 });
