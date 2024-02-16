7a48341965da7eb6904d700c8f96fe1e7bd014f4
Reinhold Schachner <reinhold.schachner@hotmail.com>
2024-02-16 12:14:31 +0100

    Create static documentation.

---
+++ site/custom.js
@@ -0,0 +1,12 @@
+function movePdocNavBar(e) {
+    navbar = document.querySelector('nav.pdoc');
+    if (!navbar) return;
+    
+    navbar.querySelector('.memberlist').classList.add('md-nav__list');
+    navbar.querySelectorAll('.memberlist a').forEach(a => a.classList.add('md-nav__link'));
+
+    sidebar = document.querySelector('.md-sidebar--secondary .md-sidebar__inner')
+    sidebar.innerHTML = navbar.outerHTML;
+    navbar.remove();
+}
+document.addEventListener("DOMContentLoaded", movePdocNavBar);
